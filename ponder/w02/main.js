3956b79381d7d08fa0233880039fd538ff9b1a5e
Leo Vidal <vidaldl@hotmail.com>
2025-09-24 23:13:36 -0600

    w02 ponder

--- ponder/w02/main.js
+++ ponder/w02/main.js
@@ -1,21 +1,19 @@
-// filepath: /Users/leovidal/Desktop/WDD 131/repo/ponder/w02/dom-basics.js
-// Step 1: Add a paragraph with JavaScript
+
 const newParagraph = document.createElement("p");
 newParagraph.innerText = "Added with Javascript!";
 document.body.appendChild(newParagraph);
 
-// Step 2: Add an image
 const newImage = document.createElement("img");
 newImage.setAttribute("src", "https://picsum.photos/200");
 newImage.setAttribute("alt", "Random image from picsum");
 document.body.appendChild(newImage);
 
-// Step 3: Add a div with complex HTML
+
 const newDiv = document.createElement("div");
 newDiv.innerHTML = "<ul><li>One</li><li>Two</li><li>Three</li></ul>";
 document.body.appendChild(newDiv);
 
-// Step 4: Create a new section with h2 and paragraph
+
 const newSection = document.createElement("section");
 const newH2 = document.createElement("h2");
 newH2.innerText = "DOM Basics";
